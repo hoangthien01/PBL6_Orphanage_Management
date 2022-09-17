0801f4e2fe15a057f37dfa82dfdf79a4439ed82a
Thien Ho <thienho549@gmail.com>
2022-09-18 01:33:50 +0700

    Add Auth guard

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,26 @@
+﻿import { Injectable } from '@angular/core';
+import {
+    Router,
+    CanActivate,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot,
+} from '@angular/router';
+//
+import { UserStorage } from '@app/core/store/user/user.storage';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+    constructor(private router: Router) {
+    }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        if (UserStorage.isLoggedIn()) {
+            return true;
+        }
+        //
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } }).then();
+        return false;
+    }
+}

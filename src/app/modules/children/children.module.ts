a6483fb9d5e0d585943b187c23d26ca8945d4f81
Thien Ho <thienho549@gmail.com>
2022-10-02 21:54:12 +0700

    Children: build add child component

--- src/app/modules/children/children.module.ts
+++ src/app/modules/children/children.module.ts
@@ -7,14 +7,16 @@ import { ThemeModule } from '@app/theme/theme.module';
 import { SvgIconsRegistry } from '@app/core/services';
 import { AuthGuard } from '@app/core/guards';
 import { ChildrenManagementComponent } from './children.component';
-import { ChildAddComponent } from './components/prospect-add/child-add.component';
+import { ChildAddComponent } from './components/child-add/child-add.component';
+import { ChildDetailComponent } from './components/child-detail/child-detail.component';
 //
 const SVG_ICONS = [
 ];
 
 const COMPONENTS = [
   ChildrenManagementComponent,
-  ChildAddComponent
+  ChildAddComponent,
+  ChildDetailComponent
 ];
 
 export const routes: Routes = [

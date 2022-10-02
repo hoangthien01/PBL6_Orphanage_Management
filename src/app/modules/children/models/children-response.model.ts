313adf75aa9512430b9168ed2c3e20b8659f5494
Thien Ho <thienho549@gmail.com>
2022-10-02 21:54:31 +0700

    Children: build detail child component

--- src/app/modules/children/models/children-response.model.ts
+++ src/app/modules/children/models/children-response.model.ts
@@ -1,10 +1,6 @@
 import { ChildrenModel } from "./children.model";
 
 export class ListChildrenResponseModel {
-  links: {
-    previous: string,
-    next: string
-  };
   current: string;
   page_size: string;
   page_number: number;
--- src/app/modules/children/services/children-management.service.ts
+++ src/app/modules/children/services/children-management.service.ts
@@ -1,3 +1,4 @@
+import { ChildrenModel } from './../models/children.model';
 import { Injectable } from '@angular/core';
 import { BaseService } from '@app/core/services';
 import { Observable } from 'rxjs';
@@ -20,16 +21,18 @@ export class ChildrenService {
     name?: string,
     age?: number,
     gender?: number,
-    status?: number,
+    status?: string,
   }): Observable<ListChildrenResponseModel> {
-    console.log('data', data);
+    return this.baseService.get(`${this.userURL}?page=${data.page}&page_size=${data.pageSize}&name=${data.name}&age=${data.age}&gender=${data.gender}&status=${data.status}`);
+  }
 
-    return this.baseService.get(`${this.userURL}?
-      page=${data.page}
-      &page_size=${data.pageSize}
-      &name=${data.name}
-      &age=${data.age}
-      &gender=${data.gender}
-      &status=${data.status}`);
+  addChild(child: any) :Observable<ChildrenModel> {
+    console.log('child', child);
+    const form = new FormData();
+    form.append('name', child.name);
+    form.append('age', child.age.toString());
+    form.append('gender', child.gender.toString());
+    form.append('personal_picture', child.personal_picture);
+    return this.baseService.post(`${this.userURL}`, form);
   }
 }

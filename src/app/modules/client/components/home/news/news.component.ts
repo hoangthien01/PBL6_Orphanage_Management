36980a16e2b09bf17f8c63df7c49e624acbebbae
Thien Ho <thienho549@gmail.com>
2022-10-28 20:44:24 +0700

    Apply api send info (not logged in)

--- src/app/modules/client/components/home/news/news.component.ts
+++ src/app/modules/client/components/home/news/news.component.ts
@@ -1,3 +1,4 @@
+import { finalize } from 'rxjs/operators';
 import { ActivityModel } from './../../../models/activity.model';
 import { Component, OnDestroy } from '@angular/core';
 import { ActivityService } from '@app/modules/client/services/activity.service';
@@ -7,6 +8,9 @@ import * as ActivitiesActions from '../../../../../core/store/activities/activit
 import { Store } from '@ngxs/store';
 import { Observable, Subscription } from 'rxjs';
 import { ActivitiesSelectors } from '@app/core/store/activities/activities.selectors';
+import { UserService } from '@app/modules/account-setting/services/user.service';
+import { SendInfoModel } from '@app/modules/client/models/send-info.model';
+import { AppNotify } from '@app/utilities';
 
 @Component({
   selector: 'app-news',
@@ -22,12 +26,14 @@ export class NewsComponent implements OnDestroy {
   page: number = 1;
   page_size: number = 6;
   type: string = 'all';
+  sendData: SendInfoModel = new SendInfoModel();
   //
   isShowRegisterPopup: boolean = false;
   //
   private _subscriptions: Subscription = new Subscription();;
   //
   constructor(private activityService: ActivityService,
+              private userService: UserService,
               private router: Router,
               private store: Store) {
   }
@@ -78,6 +84,15 @@ export class NewsComponent implements OnDestroy {
   }
 
   sendInfo() {
+    this.userService.sendInfo(this.sendData)
+    .pipe(
+      finalize(() => {
+
+      })
+    )
+    .subscribe((res) => {
+				AppNotify.success('Đăng kí nhận thông tin thành công.');
+    })
     this.isShowRegisterPopup = false;
   }
 

593de7fa8e616b1b62abe067906ee6b65e9de6cd
Thien Ho <thienho549@gmail.com>
2022-11-05 00:49:59 +0700

    Update donate payment

--- src/app/modules/client/components/donate/donate.component.ts
+++ src/app/modules/client/components/donate/donate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
+import {DonateService} from "@app/modules/client/services/donate.service";
 
 @Component({
   selector: 'app-client-donate',
@@ -9,7 +10,9 @@ export class DonateComponent implements OnDestroy {
   paymentHandler: any = null;
   money: number = 10000;
   //
-  constructor() {}
+  constructor(private _donateService: DonateService) {
+      this.makePayment = this.makePayment.bind(this);
+  }
 
   ngOnInit() {
     this.invokeStripe();
@@ -18,13 +21,29 @@ export class DonateComponent implements OnDestroy {
   ngOnDestroy(): void {
   }
 
-  makePayment(amount: any) {
+  payment() {
+      // if (this.makePayment()) {
+      //
+      // };
+  }
+
+  makePayment(amount: number) {
     const paymentHandler = (<any>window).StripeCheckout.configure({
       key: 'pk_test_51M066MIV4V5C5hgG2RRmkFGiwNiCTyvf1OV9zVDYvhbPxlAuAKs7K8QCtK6qbGN4HYuJsxopcCGwmCUrJ848pvNy00Bco1q7uF',
       locale: 'auto',
       token: function (stripeToken: any) {
         console.log(stripeToken);
-        alert('Stripe token generated!');
+        if (stripeToken) return true;
+        const data = {
+            activity: '11111',
+            amount: this.money
+        }
+        this._donateService.donate(data).subscribe(
+            res => {
+              console.log(res);
+          }
+        )
+        // alert('Stripe token generated!');
       },
     });
     paymentHandler.open({
@@ -33,6 +52,7 @@ export class DonateComponent implements OnDestroy {
       amount: amount * 100,
     });
   }
+
   invokeStripe() {
     if (!window.document.getElementById('stripe-script')) {
       const script = window.document.createElement('script');
@@ -45,7 +65,7 @@ export class DonateComponent implements OnDestroy {
           locale: 'auto',
           token: function (stripeToken: any) {
             console.log(stripeToken);
-            alert('Payment has been successfull!');
+            // alert('Payment has been successfull!');
           },
         });
       };

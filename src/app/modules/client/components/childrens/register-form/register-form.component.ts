e30edf6e894ea7531c1a3dc3b7734a3307eb580d
Thien Ho <thienho549@gmail.com>
2022-11-06 22:06:02 +0700

    Fill auth result register children form

--- src/app/modules/client/components/childrens/register-form/register-form.component.ts
+++ src/app/modules/client/components/childrens/register-form/register-form.component.ts
@@ -3,6 +3,10 @@ import {ChildrenModel} from "@app/modules/children/models";
 import {GENDER_TYPES} from "@app/shared/app.constants";
 import {ChildrenService} from "@app/modules/children/services/children-management.service";
 import {finalize} from "rxjs/operators";
+import {UserLoggedInModel} from "@app/core/store/models";
+import {Observable, Subscription} from "rxjs";
+import {Select} from "@ngxs/store";
+import {UserSelectors} from "@app/core/store";
 
 @Component({
     selector: 'app-children-register',
@@ -11,7 +15,10 @@ import {finalize} from "rxjs/operators";
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RegisterChildrenComponent implements OnDestroy, OnInit {
+    @Select(UserSelectors.userLogged) userLogged$: Observable<UserLoggedInModel>;
+    //
     childrenDataSource: ChildrenModel[];
+    currentUser: UserLoggedInModel = new UserLoggedInModel();
     //
     pagingSize: number = 10;
     pageIndexDefault: number = 0;
@@ -28,11 +35,17 @@ export class RegisterChildrenComponent implements OnDestroy, OnInit {
         'Sống riêng'
     ]
     //
+    private _subscription : Subscription = new Subscription();
+    //
     constructor(private childrenService: ChildrenService,
                 private changeDetector: ChangeDetectorRef) {
     }
 
     ngOnInit(): void {
+        this._subscription.add(this.userLogged$.subscribe((res) => {
+            this.currentUser = res;
+            this.changeDetector.detectChanges();
+        }));
         this.getListChildrens();
     }
 

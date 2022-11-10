e95438d80714e725d2ea273da140c3f7436e6aea
Thien Ho <thienho549@gmail.com>
2022-11-10 09:35:08 +0700

    style UI

--- src/app/modules/client/components/home/footer/footer.component.ts
+++ src/app/modules/client/components/home/footer/footer.component.ts
@@ -1,4 +1,11 @@
+import { UserService } from './../../../../account-setting/services/user.service';
 import { Component, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngxs/store';
+import { UserSelectors } from '@app/core/store';
+import { AppNotify } from '@app/utilities';
+import { Observable, Subscription } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-client-footer',
@@ -6,9 +13,35 @@ import { Component, OnDestroy } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnDestroy {
-  constructor() {
+  isRegisteredInfo$: Observable<boolean> = this.store.select<boolean>(UserSelectors.isUserRegisteredInfo);
+  //
+  isRegisteredInfo: boolean = false;
+  //
+  private _subscriptions = new Subscription();
+  //
+  constructor(private router: Router,
+              private store: Store,
+              private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this._subscriptions.add(this.isRegisteredInfo$.subscribe((isRegisteredInfo: boolean) => {
+        this.isRegisteredInfo = isRegisteredInfo;
+    }));
   }
 
   ngOnDestroy(): void {
+  }
+
+  goDonatePage() {
+    this.router.navigateByUrl('/donate', {state: {activityId: ''}}).then();
+  }
+
+  sendInfo() {
+    this.userService.sendInfo(null)
+    .subscribe((res) => {
+        this.isRegisteredInfo = true;
+        AppNotify.success('Đăng kí nhận thông tin thành công.');
+    })
   }
 }

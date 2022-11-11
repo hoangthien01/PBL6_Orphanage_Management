7909127bb65db4fd70a1488cc5d0e19042bdf7a6
Thien Ho <thienho549@gmail.com>
2022-11-11 23:56:38 +0700

    Fix error wrong family status

--- src/app/modules/client/data/const/family-status.const.ts
+++ src/app/modules/client/data/const/family-status.const.ts
@@ -3,7 +3,7 @@ import { familyStatus } from "../enum/family-status.enum";
 export const FAMILY_STATUS : {key: number, value: string}[] = [
     {
         key: familyStatus.Separately,
-        value: 'Sống chung',
+        value: 'Sống riêng',
     },
     {
         key: familyStatus.Together,
